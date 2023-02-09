3131decd451b192d63ae25681ab0ee562c34d40f
Sonui <me@sonui.cn>
2023-02-09 13:52:17 +0000

    ci: test

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+root=$(pwd)
+cd /
 PROTOC_ZIP=protoc-21.12-linux-x86_64.zip
 curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v21.12/$PROTOC_ZIP
-sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
-sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
+unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
 rm -f $PROTOC_ZIP
 
-root=$(pwd)
+cd $root
 cd Feed/proto
 protoc *.proto --go_out=./ --go-grpc_out=./
 
